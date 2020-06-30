b2d931ae83131f806645e1c1b580883f4558d755
Harold G Luzardo B <haroldburtonl@gmail.com>
2020-06-30 19:13:49 -0400

    First commit
    Up to here, we are just checking the Linux version

---
+++ shorewall _install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Detect OS
+# $os_version variables aren't always in use, but are kept here for convenience
+if grep -qs "ubuntu" /etc/os-release; then
+	os="ubuntu"
+	os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
+    apt-get update
+    apt-get upgrade -y
+	apt-get install git build-essential wget shorewall -y
+elif [[ -e /etc/debian_version ]]; then
+	os="debian"
+	os_version=$(grep -oE '[0-9]+' /etc/debian_version | head -1)
+    apt-get update
+    apt-get upgrade -y
+	apt-get install git build-essential wget shorewall -y
+elif [[ -e /etc/centos-release ]]; then
+	os="centos"
+	os_version=$(grep -oE '[0-9]+' /etc/centos-release | head -1)
+    yum install gcc gcc-c++ kernel-devel make wget git -y
+    cd /root
+    wget http://www.invoca.ch/pub/packages/shorewall/RPMS/ils-8/noarch/shorewall-5.2.5.2-1.el8.noarch.rpm
+    rpm -qpR shorewall-5.2.5.2-1.el8.noarch.rpm
+    rpm -i shorewall-5.2.5.2-1.el8.noarch.rpm
+
+elif [[ -e /etc/fedora-release ]]; then
+	os="fedora"
+	os_version=$(grep -oE '[0-9]+' /etc/fedora-release | head -1)
+	yum install gcc gcc-c++ kernel-devel make
+    yum install gcc gcc-c++ kernel-devel make wget git -y
+    cd /root
+    wget http://www.invoca.ch/pub/packages/shorewall/RPMS/ils-8/noarch/shorewall-5.2.5.2-1.el8.noarch.rpm
+    rpm -qpR shorewall-5.2.5.2-1.el8.noarch.rpm
+    rpm -i shorewall-5.2.5.2-1.el8.noarch.rpm
+else
+	echo "This installer seems to be running on an unsupported distribution.
+Supported distributions are Ubuntu, Debian, CentOS, and Fedora."
+	exit
+fi
