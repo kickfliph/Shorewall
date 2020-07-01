c2eda29fda018f02c41056a96502397a087e1cd6
Harold Luzardo <haroldburtonl@gmail.com>
2020-06-30 20:02:04 -0400

    adding the core RPM for shorewall

--- shorewall_install.sh
+++ shorewall_install.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-# Detect OS
+# detect os
 # $os_version variables aren't always in use, but are kept here for convenience
 if grep -qs "ubuntu" /etc/os-release; then
 	os="ubuntu"
-	os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
+	os_version=$(grep 'version_id' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
     apt-get update
     apt-get upgrade -y
 	apt-get install git build-essential wget shorewall -y
 elif [[ -e /etc/debian_version ]]; then
 	os="debian"
-	os_version=$(grep -oE '[0-9]+' /etc/debian_version | head -1)
+	os_version=$(grep -oe '[0-9]+' /etc/debian_version | head -1)
     apt-get update
     apt-get upgrade -y
 	apt-get install git build-essential wget shorewall -y
 elif [[ -e /etc/centos-release ]]; then
 	os="centos"
-	os_version=$(grep -oE '[0-9]+' /etc/centos-release | head -1)
-    yum install gcc gcc-c++ kernel-devel make wget git perl-Digest-SHA.x86_64.rpm -y
+	os_version=$(grep -oe '[0-9]+' /etc/centos-release | head -1)
+    yum install gcc gcc-c++ kernel-devel make wget git perl-digest-sha.x86_64.rpm -y
     cd /root
-
-    wget http://www.invoca.ch/pub/packages/shorewall/RPMS/ils-8/noarch/shorewall-5.2.5.2-1.el8.noarch.rpm
-    rpm -qpR shorewall-5.2.5.2-1.el8.noarch.rpm
+    wget http://www.invoca.ch/pub/packages/shorewall/rpms/ils-8/noarch/shorewall-core-5.2.5.2-1.el8.noarch.rpm
+    wget http://www.invoca.ch/pub/packages/shorewall/rpms/ils-8/noarch/shorewall-5.2.5.2-1.el8.noarch.rpm
+    rpm -i shorewall-core-5.2.5.2-1.el8.noarch.rpm
     rpm -i shorewall-5.2.5.2-1.el8.noarch.rpm
-    
 elif [[ -e /etc/fedora-release ]]; then
 	os="fedora"
 	os_version=$(grep -oE '[0-9]+' /etc/fedora-release | head -1)
